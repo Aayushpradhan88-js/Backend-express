753c9b7e676840a625541ea83b12c1f131336cf2
Aayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-05-15 15:26:04 +0000

    making routes

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const express = require('express') //First method
 
 const app = express()      //Note:- In express app is very powerful. Because it can access all the power of express
 
-const port = 4000         //It is an virtual port we can set as our prefrences like - 4000 or 1000 ...etc
+app.use(function(req, res, next) {
+  console.log(req);
+})
+
+app.get('/youtube', (req, res) => {
+  res.get('Open youtube')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/github', (req, res) => {
-    res.send('aayushdotcom')
+  res.send('aayushdotcom')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+const port = 4000         //It is an virtual port we can set as our prefrences like - 4000 or 1000 ...etc
