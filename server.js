fd35a1ec603b7a7d1552f6550dbd4c8e79899052
Aayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-05-16 06:50:51 +0000

    adding removed port 3000 code

--- server.js
+++ server.js
@@ -3,22 +3,33 @@ const express = require('express') //First method
 
 const app = express()      //Note:- In express app is very powerful. Because it can access all the power of express
 
-app.use(function(req, res, next) {
-  console.log(req);
-})
+// app.use(function(req, res, next) => {
+//   console.log(req)
+//   next()
+// })
 
 app.get('/youtube', (req, res) => {
   res.get('Open youtube')
 })
-
+ 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.get('hello world');
 })
 
 app.get('/github', (req, res) => {
   res.send('aayushdotcom')
 })
 
+app.get('/profile', (req, res) => {
+  res.send('hello aayush');
+})
 
+app.get('/profile/username', (req, res) =>{
+  res.send('hello abishpradhan');
+})
 
-const port = 4000         //It is an virtual port we can set as our prefrences like - 4000 or 1000 ...etc
+const port = 3000;//It is an virtual port we can set as our prefrences like - 4000 or 1000 ...etc
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
