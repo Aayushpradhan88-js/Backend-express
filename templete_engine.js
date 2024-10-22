ed06e2928d3a9c91a8a25a76871aaeff988a55ac
aayush pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-10-22 10:20:15 +0545

    Update templete_engine.js

--- templete_engine.js
+++ templete_engine.js
@@ -3,10 +3,12 @@ const app = express()
 
 app.set("viewengine","ejs")
 
+//Routing 
 app.get('/', (req, res) => {
   res.send('index')
 })
 
+//Virtual port at local machine
 const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
